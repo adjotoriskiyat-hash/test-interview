cfbc157b48c858d0eb3cafb5bb41a217fe931705
adjotoriskiyat-hash <adjotoriskiyat@gmail.com>
2026-09-02 20:59:03 +0100

    Update vercel configuration

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/question1', async (req, res) => {
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 }
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 });
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-                                                                app.listen(3000, () => {
-                                                                    console.log('Server is running on port 3000');
-                                                                    });
-                                                                    
+                                                         app.listen(3000, () => {
+                                                          console.log('Server is running on port 3000');
+                                                          });
+module.exports = app;
